d51fab65614bbe63e950ec584d4d12f742d44b17
ANDREI PETRUK - Student <G00328108@gmit.ie>
2016-10-05 10:57:32 +0100

    name is

--- hello.py
+++ hello.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+	
+@app.route('/name')
+def name():
+    return 'Your name is:'	
+	
 
 if __name__ == "__main__":
     app.run()
+
+
+
